dd5ff58cd615a9932a062871c675687738d602e9
DannyBoy <neo3nt3r@gmail.com>
2019-12-06 17:04:16 -0300

    Update pykipedia.py

--- pykipedia.py
+++ pykipedia.py
@@ -1,22 +1,28 @@
 import requests as r
 from bs4 import BeautifulSoup
 import string
+import sys,os
 
+# Import all the things
 assunto = input('Qual o assunto? ~>')
 assunto = assunto.strip()
 assunto = assunto.replace(" ", "_")
-print(assunto)
+# String treatment
 page = r.get('https://pt.wikipedia.org/wiki/{}'.format(assunto))
 soup = BeautifulSoup(page.text, 'html.parser')
+# Parser
 p = soup.find_all('p')
+i = soup.find_all('i')
+li = soup.find_all('li')
+span = soup.find_all('span')
+# <> stuff
+print('Consultando wikipedia...\n')
+os.system('sleep 3')
+# Begin loop
 c = 0
 while c < len(p):
-  print(soup.find_all('p')[c].get_text())
+  print(p[c].get_text())
   c += 1
-  if c == len(p):
-    print(soup.find_all('i')[c].get_text())
-    print(soup.find_all('li')[c].get_text())
-    print(soup.find_all('a')[c].get_text())
-
+#End loop
 
 
